4611929c8bf536b9c8faff4196ebef5621066448
Ali Nauroze <alinauroze@hotmail.com>
2019-12-28 23:23:35 +0500

    HSM server without express

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const exec = require('child_process').execFile;
-const express = require('express');
+const http = require('http');
+const queryString = require('querystring');
+const URL = require('url');
 
-const app = express();
+http.createServer((req, res) => {
+	let url = URL.parse(req.url);
+	
+	req.query = queryString.parse(url.query);
+	res.writeHead(200, {"Content-type" : "application/json"});
+	
+	if (url.pathname == "/encrypt") {
+		exec("des.exe", ["encrypt", req.query.text], {cwd : "exes"}, (err, res_) => {
+			res_ = JSON.parse(res_);
+			
+			if (err)
+				res.end('{"err" : true, "msg" : res_.data}');
+			
+			res.end(JSON.stringify(res_));
+		})
+	}
+	else if (url.pathname == "/decrypt") {
+		exec("des.exe", ["decrypt", req.query.cipher], {cwd : "exes"}, (err, res_) => {
+			res_ = JSON.parse(res_);
+			
+			if (err)
+				res.send({"err" : true, "msg" : res_.data});
+			
+			res.end(JSON.stringify(res_));
+		})
+	}
+	else {
+		res.end('{"err" : true, "msg" : "no route found"}');
+	}
 
+}).listen(3005);
+
+/*
 app.get("/encrypt", (req, res) => {
 	exec("des.exe", ["encrypt", req.query.text], {cwd : "exes"}, (err, res_) => {
 		res_ = JSON.parse(res_);
@@ -24,5 +57,4 @@ app.get("/decrypt", (req, res) => {
 		res.send(res_);
 	})
 });
-
-app.listen(3005);
+*/
